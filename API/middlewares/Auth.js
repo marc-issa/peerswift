140c765509801b2af50ec45f09c9383e0d667572
Marc Issa <issamarc15@gmail.com>
2024-03-18 23:40:27 +0200

    Implementing otp verification and phone verification

--- API/middlewares/Auth.js
+++ API/middlewares/Auth.js
@@ -4,7 +4,6 @@ module.exports = {
 	authenticateJWT: (req, res, next) => {
 		const authHeader = req.headers["authorization"];
 		const token = authHeader && authHeader.split(" ")[1];
-		console.log(token);
 
 		if (token == null) return res.sendStatus(401);
 
